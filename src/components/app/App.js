2e2e1ab688565360a0777f57a8a7798ff11d033f
matthiashaefeli <haefeli.matthias@gmail.com>
2023-02-12 08:38:05 -0600

    yarn update, switch replaced with routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -7,7 +7,7 @@ import Offer from '../offer/Offer';
 import News from '../news/News';
 import Foto from '../foto/Foto';
 import Place from '../place/Place';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import Main from '../main/Main';
 import './app.css'
 
@@ -17,16 +17,16 @@ class App extends Component {
     return (
       <div className='mainDiv'>
         <Main />
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/contact' component={Contact} />
-          <Route exact path='/daily' component={Daily} />
-          <Route exact path='/location' component={Location} />
-          <Route exact path='/offer' component={Offer} />
-          <Route exact path='/news' component={News} />
-          <Route exact path='/fotos' component={Foto} />
-          <Route exact path='/place' component={Place} />
-        </Switch>
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route exact path='/contact' element={<Contact/>} />
+          <Route exact path='/daily' element={<Daily/>} />
+          <Route exact path='/location' element={<Location/>} />
+          <Route exact path='/offer' element={<Offer/>} />
+          <Route exact path='/news' element={<News/>} />
+          <Route exact path='/fotos' element={<Foto/>} />
+          <Route exact path='/place' element={<Place/>} />
+        </Routes>
       </div>
     );
   }
