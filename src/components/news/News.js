39b15bc505c202fe04a37dd1cfb0bb9917491e2b
matthiashaefeli <haefeli.matthias@gmail.com>
2024-09-17 18:44:40 -0500

    spelling

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -10,14 +10,16 @@ class News extends Component {
           Für die Zukunft unserer Kinder - Nachhaltigkeit im Wichtelhuus
         </p>
         <p>
-          Im Wichtelhuus legen wir nicht nur grossen Wert auf die Betreuung unserer Kinder,
+          Im Wichtelhuus legen wir nicht nur großen Wert auf die Betreuung unserer Kinder,
           sondern auch auf ihre Zukunft. Durch die Zusammenarbeit mit Apalife kompensieren wir den Stromverbrauch und den Abfall,
-          der in unserer Spielgruppe anfällt. So tragen wir aktiv zum Klimaschutz bei und reduzieren den CO2-Ausstoss.
+          der in unserer Spielgruppe anfällt.
+          So tragen wir aktiv zum Klimaschutz bei und reduzieren den CO₂-Ausstoß
         </p>
         <p>
-          Warum tun wir das? Uns liegt es am Herzen, dass die Welt, in der unsere Kinder aufwachsen,
-          auch für zukünftige Generationen lebenswert bleibt. Im Wichtelhuus übernehmen wir Verantwortung, damit die Kinder,
-          die wir heute betreuen, auch morgen in einer gesunden und nachahltigen Umwelt spielen und lernen können.
+          Warum tun wir das? Uns liegt es am Herzen, dass die Welt,
+          in der unsere Kinder aufwachsen, auch für zukünftige Generationen lebenswert bleibt.
+          Im Wichtelhuus übernehmen wir Verantwortung, damit die Kinder,
+          die wir heute betreuen, auch morgen in einer gesunden und nachhaltigen Umwelt spielen und lernen können.
         </p>
         <p>
           Gemeinsam schaffen wir eine bessere Zukunft für unsere Kinder im Wichtelhuus!
